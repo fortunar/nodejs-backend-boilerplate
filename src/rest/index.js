4fc17e7dcdf1bae8c349426c858963ad33ea06f1
Rok Fortuna <rokfortuna@Roks-MacBook-Pro.local>
2015-12-01 15:53:56 +0100

    Minor refactoring, included passport plugin.

--- src/rest/index.js
+++ src/rest/index.js
@@ -1,9 +1,32 @@
 import models from '../../models'
 import epilogue from 'epilogue'
 import {auth, logger} from './../interceptors'
+import dbconfig from './../../config/DBConfig'
+import express from 'express'
+import login from './../login'
 
 
-export default function(){
+export default function(App){
+
+  epilogue.initialize({
+    app: App,
+    sequelize: dbconfig
+  });
+
+  const routes = express.Router();
+
+  routes.get('/', function(req,res){
+    var token = jwt.sign({user:"marko"}, "secret", {
+            expiresInMinutes: 2
+          });
+    res.json({
+      user:"bano",
+      token: token
+    })
+  });
+
+
+  App.use('/login', routes);
 
   var users = epilogue.resource({
     model: models.users,
